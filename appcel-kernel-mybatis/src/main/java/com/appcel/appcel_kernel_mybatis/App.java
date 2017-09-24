407e1b86f3774bff61fbad6501b71692609f0d80
DukeXia <xiadengkuan@163.com>
2017-09-24 11:16:15 +0800

    添加配置和持久层工程

---
+++ appcel-kernel-mybatis/src/main/java/com/appcel/appcel_kernel_mybatis/App.java
@@ -0,0 +1,13 @@
+package com.appcel.appcel_kernel_mybatis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
